1f4f3acd2ed0855547166774429776aad0ff390d
msmalinosterberg <msmalinosterberg@gmail.com>
2021-03-09 16:50:25 +0100

    footer with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import './App.css';
+import React from 'react';
+import './App.css'
+import Footer2 from './componenets/Footer';
 import Navbar from './componenets/Navbar';
 import StartPageView from './componenets/StartPage/StartPageView';
 
@@ -7,6 +9,7 @@ function App() {
     <>
       <Navbar /> 
       <StartPageView />
+      <Footer2 /> 
     </>
   );
 }
--- src/componenets/Footer.tsx
+++ src/componenets/Footer.tsx
@@ -1,1 +1,32 @@
-export{}
+import { Row, Col } from 'antd';
+import Layout, { Footer } from 'antd/lib/layout/layout';
+import React, { CSSProperties } from 'react'; 
+import logo from '../assets/logga.png'; 
+
+
+
+function Footer2() {
+    
+    return (
+        <Layout>
+            <Row style={{width: '100%'}}>
+            <Col span={24}>
+            <Footer style = {{background: 'black', height: '6rem'}}>
+            <img src={logo} alt="logo" style={logoStyle2} />
+            </Footer>
+            </Col> 
+            </Row>
+        </Layout>
+    ); 
+}
+
+
+const logoStyle2: CSSProperties = {
+     
+    display: 'flex', 
+    alignItems: 'center', 
+    justifyContent:'center',
+   
+}
+
+export default Footer2; 
