211d66f1387d3e391bdeca8b46b9be7f5e7bbeef
Moa Stenqvist <moa@stonetwig.se>
2021-03-09 15:58:50 +0100

    Add styling to card container + responsive grid and gutters.

--- src/componenets/StartPage/ProductCard.tsx
+++ src/componenets/StartPage/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, CSSProperties } from 'react';
 import { Card, Col, List, Row } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import { productList } from '../ProductItemsList';
@@ -9,35 +9,52 @@ class ProductCard extends Component {
     
     render() {
         return(
-            <List
-                grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 2,
-                    md: 2,
-                    lg: 4,
-                    xl: 4,
-                    xxl: 4,
-                }}
-                dataSource={productList}
-                renderItem={item => (
-                    <List.Item>
-                        <Card
-                            hoverable
-                            style={{ width: 240 }}
-                            cover={<img src={item.imageUrl} />}
-                            actions={[
-                                <ShoppingCartOutlined style={{ fontSize: '2rem' }}/>
-                            ]}
-                        >
-                            <Meta title={item.title} description={item.price + ' kr'} />
-                        </Card>
-                    </List.Item>
-                )}
-                
-            />
-    )
+            <Row style={cardContainer}>
+                <Col span={24} style={columnStyle}>
+                    <List
+                        grid={{
+                            gutter: 25,
+                            xs: 1,
+                            sm: 2,
+                            md: 2,
+                            lg: 4,
+                            xl: 4,
+                            xxl: 4,
+                        }}
+                        dataSource={productList}
+                        renderItem={item => (
+                            <List.Item>
+                                <Card
+                                    hoverable
+                                    cover={<img src={item.imageUrl} />}
+                                    actions={[
+                                        <ShoppingCartOutlined style={{ fontSize: '2rem' }}/>
+                                    ]}
+                                >
+                                    <Meta title={item.title} description={item.price + ' kr'} />
+                                </Card>
+                            </List.Item>
+                        )}    
+                    />
+                </Col>
+            </Row>
+        )
     }
 }
 
 export default ProductCard;
+
+const cardContainer: CSSProperties = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'space-around',
+    width: '80%',
+    margin: 'auto'
+}
+const columnStyle: CSSProperties = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '3rem',
+    marginBottom: '3rem',
+}
