a60c5cff25ae45abe5c50318528ffffb4a440051
msmalinosterberg <msmalinosterberg@gmail.com>
2021-03-10 18:05:36 +0100

    added image, text and button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css'
 import Footer2 from './componenets/Footer';
 import Navbar from './componenets/Navbar';
+import ProductDetails from './componenets/ProductDetails/ProductDetails';
 import StartPageView from './componenets/StartPage/StartPageView';
 
 function App() {
   return (
     <>
       <Navbar /> 
-      <StartPageView />
+     {/* <StartPageView /> */}
+     <ProductDetails /> 
+
       <Footer2 /> 
     </>
   );
