358665a3015180e88cf9bcc31efc661eed07220e
Amanda <amanda.e.samuelsson@hotmail.com>
2021-03-10 15:07:27 +0100

    Updated path for pics and cleaned up the code

--- src/componenets/StartPage/Carousel.tsx
+++ src/componenets/StartPage/Carousel.tsx
@@ -1,8 +1,5 @@
 import React, { Component, CSSProperties } from 'react';
 import { Carousel } from 'antd';
-import carousel1 from '/Users/AmandasMacbook/Documents/Medieinstitutet HT20/React/miniprojekt-webbshop/src/assets/carousel1.png';
-
-
 
 class CarouselStart extends Component {
 
@@ -11,16 +8,25 @@ class CarouselStart extends Component {
 
             <Carousel autoplay>
                 <div>
-                    <img src={carousel1} alt="girl with glasses" style={caoruselPic1} />
-                    <h1 style={carouselStyle}>1</h1>
+                    <img 
+                        src="https://github.com/msmalinosterberg/miniprojekt/blob/master/src/assets/carousel1.png?raw=true" 
+                        alt="girl with sunglasses" 
+                        style={caoruselPic} 
+                    />
                 </div>
                 <div>
-                    <img src={carousel1} alt="girl with glasses" style={caoruselPic1} />
-                    <h3 style={carouselStyle}>2</h3>
+                    <img 
+                        src="https://github.com/msmalinosterberg/miniprojekt/blob/master/src/assets/carousel1.png?raw=true" 
+                        alt="girl with sunglasses" 
+                        style={caoruselPic} 
+                    />
                 </div>
                 <div>
-                    <img src={carousel1} alt="girl with glasses" style={caoruselPic1} />
-                    <h3 style={carouselStyle}>3</h3>
+                    <img 
+                        src="https://github.com/msmalinosterberg/miniprojekt/blob/master/src/assets/carousel1.png?raw=true" 
+                        alt="girl with sunglasses" 
+                        style={caoruselPic} 
+                    />
                 </div>
             </Carousel>
         )
@@ -30,23 +36,11 @@ class CarouselStart extends Component {
 
 export default CarouselStart;
 
-const carouselStyle: CSSProperties = {
-    display: 'flex',
-    fontSize: '16px',
-    background: 'pink'
-};
-const caoruselPic1: CSSProperties = {
+const caoruselPic: CSSProperties = {
     display: 'flex',
     height: 'auto',
     width: '100%'
 };
-// const carouselDiv: CSSProperties = {
-//     display: 'flex',
-//     backgroundImage: 'url(../asssets/prod2.png)',
-//     height: '100rem',
-//     backgroundPosition: 'center',
-//     backgroundSize: 'cover',
-//     backgroundRepeat: 'no-repeat',
-// };
+
 
 
