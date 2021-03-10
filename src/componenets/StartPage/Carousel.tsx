dcc44e7e0205c6543d45a5e99e0c029e94dcfe84
Amanda <amanda.e.samuelsson@hotmail.com>
2021-03-10 14:45:08 +0100

    Added pictures to the carousel

--- src/componenets/StartPage/Carousel.tsx
+++ src/componenets/StartPage/Carousel.tsx
@@ -1,6 +1,6 @@
 import React, { Component, CSSProperties } from 'react';
 import { Carousel } from 'antd';
-import carousel1 from '../assets/carousel1.png'; 
+import carousel1 from '/Users/AmandasMacbook/Documents/Medieinstitutet HT20/React/miniprojekt-webbshop/src/assets/carousel1.png';
 
 
 
@@ -12,22 +12,20 @@ class CarouselStart extends Component {
             <Carousel autoplay>
                 <div>
                     <img src={carousel1} alt="girl with glasses" style={caoruselPic1} />
- 
                     <h1 style={carouselStyle}>1</h1>
                 </div>
                 <div>
+                    <img src={carousel1} alt="girl with glasses" style={caoruselPic1} />
                     <h3 style={carouselStyle}>2</h3>
                 </div>
                 <div>
+                    <img src={carousel1} alt="girl with glasses" style={caoruselPic1} />
                     <h3 style={carouselStyle}>3</h3>
-                </div>
-                <div>
-                    <h3 style={carouselStyle}>4</h3>
                 </div>
             </Carousel>
         )
     }
-    
+
 }
 
 export default CarouselStart;
@@ -35,12 +33,20 @@ export default CarouselStart;
 const carouselStyle: CSSProperties = {
     display: 'flex',
     fontSize: '16px',
-    height: '25rem',
     background: 'pink'
 };
 const caoruselPic1: CSSProperties = {
     display: 'flex',
-    height: '25rem',
+    height: 'auto',
+    width: '100%'
 };
+// const carouselDiv: CSSProperties = {
+//     display: 'flex',
+//     backgroundImage: 'url(../asssets/prod2.png)',
+//     height: '100rem',
+//     backgroundPosition: 'center',
+//     backgroundSize: 'cover',
+//     backgroundRepeat: 'no-repeat',
+// };
 
 
