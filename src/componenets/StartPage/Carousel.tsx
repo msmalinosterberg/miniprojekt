753c6b7ef1bc3500198effa2150d699739a927a3
Amanda <amanda.e.samuelsson@hotmail.com>
2021-03-10 14:11:59 +0100

    Added carousel to start page

--- src/componenets/StartPage/Carousel.tsx
+++ src/componenets/StartPage/Carousel.tsx
@@ -1,1 +1,46 @@
-export{}
+import React, { Component, CSSProperties } from 'react';
+import { Carousel } from 'antd';
+import carousel1 from '../assets/carousel1.png'; 
+
+
+
+class CarouselStart extends Component {
+
+    render() {
+        return (
+
+            <Carousel autoplay>
+                <div>
+                    <img src={carousel1} alt="girl with glasses" style={caoruselPic1} />
+ 
+                    <h1 style={carouselStyle}>1</h1>
+                </div>
+                <div>
+                    <h3 style={carouselStyle}>2</h3>
+                </div>
+                <div>
+                    <h3 style={carouselStyle}>3</h3>
+                </div>
+                <div>
+                    <h3 style={carouselStyle}>4</h3>
+                </div>
+            </Carousel>
+        )
+    }
+    
+}
+
+export default CarouselStart;
+
+const carouselStyle: CSSProperties = {
+    display: 'flex',
+    fontSize: '16px',
+    height: '25rem',
+    background: 'pink'
+};
+const caoruselPic1: CSSProperties = {
+    display: 'flex',
+    height: '25rem',
+};
+
+
