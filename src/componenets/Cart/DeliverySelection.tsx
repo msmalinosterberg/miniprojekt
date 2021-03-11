33a72e5db1b6aae48146bd7d04234bc751e2003f
Amanda <amanda.e.samuelsson@hotmail.com>
2021-03-11 14:46:41 +0100

    Updated

--- src/componenets/Cart/DeliverySelection.tsx
+++ src/componenets/Cart/DeliverySelection.tsx
@@ -8,25 +8,26 @@ interface State {
  
 const RadioGroup = Radio.Group;
 
-class DeliverySection extends React.Component<State> {
-  state: State = {
-    value: {}
-  }
+class DeliverySection extends React.Component {
+  state = {
+    value: 1,
+  };
   
-  onChange = (e: { target: { value: any; }; }) => {
+  onChange = (e: any) => {
     console.log('radio checked', e.target.value);
     this.setState({
       value: e.target.value,
     });
-  }    
+  };
 
   render() {
+    const { value } = this.state;
     return (
         <Content>
             <h2>
                 Delivery
             </h2>
-            <RadioGroup onChange={() => this.onChange} value={this.state.value}>
+            <RadioGroup onChange={this.onChange} value={value}>
                 <Radio value={1}>Postnord, <strong>24h - 145 kr</strong></Radio>
                 <Radio value={2}>Bring, <strong>48h - 129 kr</strong></Radio>
                 <Radio value={3}>DB Schenker, <strong>72h - 89 kr</strong></Radio>
