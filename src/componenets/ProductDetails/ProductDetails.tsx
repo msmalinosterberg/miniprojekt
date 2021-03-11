f0110598b7306f16360d47519821cae73b8fdab9
msmalinosterberg <msmalinosterberg@gmail.com>
2021-03-11 14:26:28 +0100

    layout for image,text,price& button.Not responsive

--- src/componenets/ProductDetails/ProductDetails.tsx
+++ src/componenets/ProductDetails/ProductDetails.tsx
@@ -1,21 +1,50 @@
 import { Layout, Row, Col } from 'antd';
-import React, { Component, CSSProperties } from 'react'; 
+import { Component, CSSProperties } from 'react'; 
 import { Image } from 'antd';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
-class ProductDetails extends Component {
+
+
+interface State {
+    productDetails: IProductList | undefined
+}
+interface IProductList {
+    id: number; 
+    title: string;
+    description: string;
+    price: number; 
+    imageUrl: string;
+}
+
+interface Props extends RouteComponentProps {
+    id: number
+}
+class ProductDetails extends Component <Props, State> {
+
+    state: State = {
+        productDetails: undefined
+    }
+    
+
+    componentDidMount() {   
+        const productId = (this.props.match.params as any).id
+
+    }
+
     render () {
 
         return (
             <Layout style={detailContainer}>
-          
                 <Row justify="center" align="top" style={{marginTop:'0.5rem'}}>
+                <div style = {divStyle}>
                     <Col lg={{span: 12}}>
                     <Image
                         width={450} 
                         style={imageStyle}
                         src='https://github.com/msmalinosterberg/miniprojekt/blob/master/src/assets/prod6.png?raw=true'/>          
-                       
                      </Col>
+                </div>
+
                     <Col lg={{span: 12}}>
                         <h2 style={titleStyle}> Dress </h2>
                         <h3 style={descriptionStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eaque ipsa asperiores blanditiis amet nisi, 
@@ -23,8 +52,6 @@ class ProductDetails extends Component {
                              sit amet consectetur adipisicing elit. Quia sequi expedita voluptates libero autem odit tempora assumenda accusamus iure,
                              exercitationem, impedit, quidem eaque. Deleniti odit quos neque officia, inventore veniam! </h3>
                         <h2 style={price}>429:-</h2>
-
-
                         <button style={{marginTop: '1rem'}}>Add to cart </button>
                     </Col>
                 </Row>
@@ -37,6 +64,14 @@ class ProductDetails extends Component {
     
 }
 
+export default withRouter(ProductDetails as any); 
+
+
+const divStyle: CSSProperties = {
+    padding: '1rem'
+   
+}
+
 const detailContainer: CSSProperties = {
     background: 'white',
     width: '100%',
@@ -45,7 +80,8 @@ const detailContainer: CSSProperties = {
     justifyContent: 'center',
     alignItems: 'center',
     color: 'black',
-   
+    padding: '1rem',
+    marginBottom: '1rem' 
     
 }
 
@@ -68,4 +104,3 @@ const price: CSSProperties = {
 }
 
 
-export default ProductDetails; 
