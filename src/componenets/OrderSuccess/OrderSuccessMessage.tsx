9271c67a63be09d136ce7f8d2993d3ed9d74063c
msmalinosterberg <msmalinosterberg@gmail.com>
2021-03-16 17:23:22 +0100

    success message.last step in cartview. button => startpage

--- src/componenets/OrderSuccess/OrderSuccessMessage.tsx
+++ src/componenets/OrderSuccess/OrderSuccessMessage.tsx
@@ -1,1 +1,19 @@
-export{}
+import { Result, Button } from 'antd';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function OrderSuccessMessage() {
+
+    return (
+        <Result
+                status="success"
+                title="You successfully purchased from FashionStore"
+                subTitle="Order number: 20171828029"
+                extra={[
+                <Link to='/'><Button type="primary" key="console">Continue shopping </Button></Link>
+            ]}
+        />
+    ) 
+}
+
+export default OrderSuccessMessage; 
