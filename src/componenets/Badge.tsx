8ca86e751dccf6a7e276fcbb24a6acc5f248b862
msmalinosterberg <msmalinosterberg@gmail.com>
2021-03-16 15:48:57 +0100

    state, get quantity

--- src/componenets/Badge.tsx
+++ src/componenets/Badge.tsx
@@ -1,27 +1,48 @@
+import { Badge, Col, Row } from "antd";
+import React, { Component } from "react";
+import { CSSProperties } from "react";
 
-import { Badge } from 'antd';
-import { Component, CSSProperties } from 'react';
 
 
-class BadgeIcon extends Component {
-    
-    
-    render() {
-        return (
-            <div>
-                <Badge style={badgeStyle}
-                    count={1}>
-                </Badge>
-              </div>
-                )
-            }
-    }
-    
-    
-const badgeStyle: CSSProperties = {
-    background: 'black',
-    border: 'white',
-    marginRight: '2.8rem',
+interface State {
+    quantity: number;
 }
 
-export default BadgeIcon; 
+class AddToBadge extends Component <State> { 
+
+    state: State = {
+       quantity: 0
+    }
+
+    getBadgeQuantity() {
+        let cartItems = JSON.parse(localStorage.getItem('cartItems') as string) || [];
+        let quantity = (cartItems
+            .map((item: any) => item.quantity)
+            .reduce((a: number, b: number) => a + b, 0)
+            ); 
+            return quantity;
+
+    } 
+        
+    render() { 
+        return (
+            <Row>
+                <Col span={24}>
+                <Badge count={0} showZero style={badgeStyle}>
+                    <a href="#" className="head-example" />
+                 </Badge>
+                </Col>
+            </Row>
+           
+        )
+    }              
+}
+
+const badgeStyle: CSSProperties = {
+    background: 'red',
+
+}
+
+export default AddToBadge;
+
+
