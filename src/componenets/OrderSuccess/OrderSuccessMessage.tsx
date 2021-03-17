92bfebb98a79cd256c1394cc93643f0e2b3bbe1d
msmalinosterberg <msmalinosterberg@gmail.com>
2021-03-17 10:23:09 +0100

    random ordernumber

--- src/componenets/OrderSuccess/OrderSuccessMessage.tsx
+++ src/componenets/OrderSuccess/OrderSuccessMessage.tsx
@@ -2,15 +2,22 @@ import { Result, Button } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 function OrderSuccessMessage() {
+
+    let orderNumber = Math.floor(Math.random() * 10256763258); 
+
+
 
     return (
         <Result
                 status="success"
                 title="You successfully purchased from FashionStore"
-                subTitle="Order number: 20171828029"
+                subTitle={"Order number:" + orderNumber}
                 extra={[
-                <Link to='/'><Button type="primary" key="console">Continue shopping </Button></Link>
+                <Link to='/'><Button type="primary" key="console" >Continue shopping  </Button></Link>
+               
             ]}
         />
     ) 
