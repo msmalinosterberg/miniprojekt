bcb0c6acad210463fa8c0ebe732d889c48c5ef84
msmalinosterberg <msmalinosterberg@gmail.com>
2021-03-22 14:50:04 +0100

    .

--- src/componenets/Admin/AddNewProduct.tsx
+++ src/componenets/Admin/AddNewProduct.tsx
@@ -2,41 +2,53 @@ import { Component, CSSProperties } from "react";
 import { Form, Input, InputNumber, Button, Col, Row } from "antd";
 import { Product } from "../ProductItemsList";
 
+
+
 const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+      number: "${label} is not a valid number!",
+    },
+    number: {
+      range: "${label} must be between ${min} and ${max}",
+    },
+  };
 
-const validateMessages = {
-  required: "${label} is required!",
-  types: {
-    email: "${label} is not a valid email!",
-    number: "${label} is not a valid number!",
-  },
-  number: {
-    range: "${label} must be between ${min} and ${max}",
-  },
-};
+  interface State {
+    products: Product[];
+    product: Product | undefined;
 
-interface State {
-  products: Product[];
-}
-
+  }
+  
 class AddNewProduct extends Component {
-  state: State = {
-    products: JSON.parse(localStorage.getItem("products") as string) || [],
-  };
+    state: State = {
+        products: JSON.parse(localStorage.getItem("products") as string) || [],
+        product: undefined,
 
-  onFinish = (values: any) => {
-    console.log(values);
-  };
-  render() {
-    return (
-      <div>
+      };
+    
+      onFinish = (values: any) => {
+        const products = JSON.parse(localStorage.getItem("products") as string) || [];
+        const newProduct: Product = {...this.state.product, ...values.product};
+        const addedProduct = products.map((item: Product) => item.id === newProduct.id ? newProduct : item);
+        localStorage.setItem('products', JSON.stringify(addedProduct));
+
+      };
+
+    render() {
+        return (
+
+    <div>
         <Row style={ContainerStyle}>
           <Col span={24} style={columnStyle}>
             <Form
@@ -52,10 +64,11 @@ class AddNewProduct extends Component {
                   fontWeight: "bold",
                 }}
               >
-                EDIT{" "}
+                ADD NEW PRODUCT {" "}
               </h1>
               <Form.Item name={["product", "name"]} label="Title">
-                <Input />
+                <Input  />
+
               </Form.Item>
 
               <Form.Item name={["product", "description"]} label="Description">
@@ -65,7 +78,7 @@ class AddNewProduct extends Component {
               <Form.Item name={["product", "price"]} label="Price">
                 <Input />
               </Form.Item>
-
+              
               <Form.Item name={["product", "imageUrl"]} label="ImageUrl">
                 <Input />
               </Form.Item>
@@ -77,28 +90,36 @@ class AddNewProduct extends Component {
                   <Button type="primary" htmlType="submit">
                     Save
                   </Button>
+
+                
                 </div>
               </Form.Item>
             </Form>
           </Col>
         </Row>
       </div>
-    );
-  }
+
+
+
+        )
+
+        
+    }
 }
 
+
 const ContainerStyle: CSSProperties = {
-  display: "flex",
-  justifyContent: "space-around",
-  alignItems: "space-around",
-  width: "60%",
-  margin: "auto",
-  height: "100vh",
-};
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "space-around",
+    width: "60%",
+    margin: "auto",
+    height: "100vh",
+  };
+  
+  const columnStyle: CSSProperties = {
+    marginTop: "6rem",
+    marginBottom: "3rem",
+  };
 
-const columnStyle: CSSProperties = {
-  marginTop: "6rem",
-  marginBottom: "3rem",
-};
-
-export default AddNewProduct;
+export default AddNewProduct; 
